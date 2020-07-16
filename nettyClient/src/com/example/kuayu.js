450ecfd8000c9e38fc95d9a0f39f7f4c99b59560
chenhao <chenhao@dhgate.com>
2020-07-17 00:52:28 +0800

    测试

--- nettyClient/src/com/example/kuayu.js
+++ nettyClient/src/com/example/kuayu.js
@@ -5,18 +5,18 @@
     </meta>
 </head>
 <body>
-<script type="text/javascript">
+<script type="text/javascript" >
     $(function(){
     $.ajax("http://www.dhgate.com/test",{
-    type: "GET",
-    success: function(data, status, xhr) {
-    alert(data.name);
-},
-    xhrFields: {
-    withCredentials: true
-},
-    crossDomain: true
-})
+        type: "GET",
+        success: function(data, status, xhr) {
+            alert(data.name);
+        },
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true
+    })
 });
 </script>
 </body>
